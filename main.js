658e5732180c9152fd68737c45843aa2eff1e0a4
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-08-18 16:17:04 -0500

    added commits function

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-function starsearch(user) {
-  repos = $.get("https://api.github.com/users/" + user + "/starred", function() {
-    console.log(repos.responseJSON);
-  })
+var data = [];
+function repo_stats(owner, repo) {
+  $.ajax({
+    datatype: "json",
+    type: "GET",
+    headers: {'Authorization':'token 5a6f90a1b19f29b38122d71818cc1dfeb0855c3b'},
+    url: "https://api.github.com/repos/" + owner + "/" + repo + "/stats/participation",
+    success: function(response) {
+      var commits = 0;
+      for(var i = 39; i<response.all.length; i++){
+        commits += response.all[i];
+      };
+      data.push(commits);
+      console.log(data);
+    }
+  });
 };
+
+function starsearch(user) {
+  repos = $.get("https://api.github.com/users/" + user + "/starred?access_token=5a6f90a1b19f29b38122d71818cc1dfeb0855c3b", function() {
+    for(var i=0; i<repos.responseJSON.length; i++){
+      project = repos.responseJSON[i];
+      owner = project.owner.login;
+      repo = project.name;
+      repo_stats(owner, repo);
+    };
+  });
+};
+
+
 
 $(document).ready(function() {
   $("#search").click(function(){
